abb893fb93186e7671ac01827e40d200e60fed49
ZZ <zhangzheng@pingcap.com>
2022-10-18 11:01:30 +0800

    fs

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
 git add .                
 git commit -m "fs"                                        
 git push -u origin master
+
+pip3 uninstall zabbix-feishu-alert
+pip3 install git+https://github.com/ezaze/zabbix-feishu-alert.git
